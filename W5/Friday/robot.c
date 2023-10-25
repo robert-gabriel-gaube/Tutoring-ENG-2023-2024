baccdea18857be5984b704c0689a5c08773fe43c
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2023-10-25 20:49:38 +0300

    Added problem description

---
+++ W5/Friday/robot.c
@@ -0,0 +1,15 @@
+/*
+Create a software solution for a robot that takes instructions as 
+input and moves based on these instructions. 
+
+An instruction is composed of 2 hexadecimal letters, the first 2 bits of the first character 
+dictate the direction in which the robot will go, the other 6 bits will represent the number 
+of spaces the robot will move in that direction. 
+
+Your job is to decrypt these instructions and to print out the positions of the robot in a 
+xoy axis system in the form x, y. The robot starts from the position 0(x), 0(y).
+
+The input stream of instructions stops when the instruction 00 is encountered.
+We are sure that the input is correct, it adheres strictly to the formulated statement.
+
+*/
