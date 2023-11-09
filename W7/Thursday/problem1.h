14d9097c1ccc6e9e87ed7857f5a9174c6090365e
Robert-Gaube <robert.gabriel.gaube@intel.com>
2023-11-09 12:56:54 +0000

    Test get_nibbles implemented

---
+++ W7/Thursday/problem1.h
@@ -0,0 +1,5 @@
+#pragma once
+
+unsigned get_nibble(unsigned number, unsigned nibblePosition);
+unsigned rotate_halves(unsigned number);
+unsigned set_nibble_at_position(unsigned number, unsigned nibble, unsigned nibblePosition);
--- W7/Thursday/testsProblem1/test_get_nibble.c
+++ W7/Thursday/testsProblem1/test_get_nibble.c
@@ -1,11 +1,22 @@
-#include "../problem1.c"
+#include "../problem1.h"
 #include <assert.h>
+#include <stdio.h>
 
 int main() {
-    unsigned number = 0x76543210;
-    
+    unsigned number = 0x76543210, passed = 0, total = 8;
+    printf("Testing get_nibble on number 0x%x\n", number);
+
     for(int idx = 0; idx < sizeof(number) * 2; ++idx) {
-        assert(idx == get_nibble(number, idx));
+        int value = get_nibble(number, idx);
+        if(idx == value) {
+            printf("\t[OK] get_nibble(number, %d) == %d\n", idx, value);
+            ++passed;
+        } else {
+            printf("\t[FAILED] get_nibble(number, %d) == %d (EXPECTED VALUE: %d)\n", idx, value, idx);    
+            
+        }
     }
+
+    printf("%d out of %d tests PASSED\n", passed, total);
     return 0;
 }
