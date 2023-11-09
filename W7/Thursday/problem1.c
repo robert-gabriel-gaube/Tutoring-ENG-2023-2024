fa71823afb6ac7a458daba4acd159d0028b8ed72
Robert-Gaube <robert.gabriel.gaube@intel.com>
2023-11-09 12:24:25 +0000

    Added problem statement

---
+++ W7/Thursday/problem1.c
@@ -0,0 +1,8 @@
+/*
+    Rotate the nibbles of a given unsigned number by moving the middle two hex symbols to the edges. 
+    For example,
+        0x12344321 becomes 0x41233214 after rotation
+        0x12345678 becomes 0x41236785. 
+        
+        The unsigned number is represented in 8 hex symbols, and each nibble is 1 hex symbol.
+*/
