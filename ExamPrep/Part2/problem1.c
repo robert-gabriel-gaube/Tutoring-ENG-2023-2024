a756bb22963bbd5b1c51fa872893871213e540ca
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2024-01-21 16:30:45 +0200

    Solved problem1

--- ExamPrep/Part2/problem1.c
+++ ExamPrep/Part2/problem1.c
@@ -6,4 +6,42 @@
     given as a parameter into its RLE form.
 
     aaabbbcc -> a3b3c2
+
+    string will be at most 500 chars. (before and after compression)
 */
+
+#include <stdio.h>
+#include <string.h>
+
+void rl_encoding(char *s) {
+    char encoded_string[500];
+    unsigned count = 1, string_len = 0; 
+
+    for(unsigned idx = 0; idx < strlen(s); ++idx) {
+        if(s[idx] == s[idx + 1]) { // If we have the same character
+            ++count;
+        } else {
+            // Put the encoded substring into encoded_string
+            // and add the \0 terminator
+            encoded_string[string_len] = s[idx];
+            encoded_string[string_len + 1] = count + '0';
+            encoded_string[string_len + 2] = '\0';
+
+            // Increment value and reset count
+            string_len += 2;
+            count = 1;
+        }
+    }
+
+    // Copy result into original string
+    strcpy(s, encoded_string);
+}
+
+int main() {
+    char s[100] = "aaabbbccd";
+    printf("BEFORE: %s\n", s);
+    rl_encoding(s);
+    printf("AFTER: %s\n", s);
+
+    return 0;
+}
