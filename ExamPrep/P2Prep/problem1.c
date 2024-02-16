c245374b1890f9efc2c3d804f18aae10f69ec134
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2024-02-16 15:52:20 +0200

    Solved

--- ExamPrep/P2Prep/problem1.c
+++ ExamPrep/P2Prep/problem1.c
@@ -8,3 +8,39 @@
         photos: 6144
         summer: 3072
 */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+long process_directory() {
+    char dir_name[50];
+    scanf("%50s", dir_name);
+
+    char token[50];
+    scanf("%50s", token);
+    scanf("%50s", token);
+
+    long count = 0;
+    while(strcmp(token, ")") != 0) {
+        if(strcmp(token, "dir") == 0) {
+            count += process_directory();
+        } else {
+            count += strtol(token, NULL, 10);
+        }
+        scanf("%50s", token);
+    }
+    printf("%s : %ld\n", dir_name, count);
+    return count;
+}
+
+int main() {
+    char word[50];
+    int read_status = scanf("%50s", word);
+    
+    
+    while(strcmp(word, "dir") == 0 && read_status == 1) {
+        process_directory();
+        read_status = scanf("%50s", word);
+    }
+    return 0;
+}
